d74d7c88a919d9c92184f5b6581f569aab047bfb
ValentinAndres <valentinandress@gmail.com>
2024-08-26 00:24:28 -0400

    Primeras page

---
+++ asistencia/src/app/professor-profile/professor-profile.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-profile',
+  templateUrl: './professor-profile.page.html',
+  styleUrls: ['./professor-profile.page.scss'],
+})
+export class ProfessorProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
